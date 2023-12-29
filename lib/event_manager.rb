9ff8616bad17296bc86038816137460117d472a0
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2023-12-29 20:54:06 +0500

    Add phone number support

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,16 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def clean_phone_number(phone_number)
+  if phone_number.length != 10 || (phone_number.length == 11 && phone_number[0] != '1')
+    "Invalid Number"
+  elsif phone_number.length == 11 && phone_number[0] == '1'
+    phone_number[1..]
+  else
+    phone_number
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -47,6 +57,9 @@ contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
+
+  phone_number = clean_phone_number(row[:homephone])
+  p phone_number
 
   legislators = legislators_by_zipcode(zipcode)
 
